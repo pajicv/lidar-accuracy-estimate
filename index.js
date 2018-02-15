a884ee4aa017db16cd6764a6d879e840b1d0ba65
pajic.vladimir <pajicv@gmail.com>
2018-02-15 12:40:47 +0100

    flightline and scan angles

--- index.js
+++ index.js
@@ -2,24 +2,26 @@
 
 const math = require('mathjs');
 
+const fs = require('fs');
+
 const lidar = require('./lib/index');
 
 const gnss = require('./lib/gnss');
 
-const deg2rad = lidar.deg2rad;
-
-/*const coordinates = lidar.calculatePoint(1000, 1000, 1000, 0, 0, 0, 200, 15, false);
-console.log('W/o errors: ' + JSON.stringify(coordinates));
-
-const coordinatesWithErrors = lidar.calculatePoint(1000, 1000, 1000, 0, 0, 0, 200, 15, true);
-console.log('With errors: ' + JSON.stringify(coordinatesWithErrors));
-
-const difference = math.subtract(coordinatesWithErrors, coordinates);
-console.log('Difference: ' + JSON.stringify(difference));*/
+const coordinatesToCsv = coordinates => coordinates.map(c => c.join(',')).join('\n');
 
 const flightline = gnss.calculateFlightLine([1000, 1000, 1000], 50, 1000, 10000);
 
 const scanAngles = gnss.calculateScanAngles(30, 1000, 50, 10000);
 
-const coordinates = flightline.map((position, i) => lidar.calculatePoint(position[0], position[1], position[2], 0, 0, 0, 200, scanAngles[i], true) );
-console.log('W/o errors: ' + JSON.stringify(coordinates));
+const coordinates = flightline.map((position, i) => lidar.calculatePoint(position[0], position[1], position[2], 0, 0, 0, 200, scanAngles[i], false) );
+
+const coordinatesWithErrors = flightline.map((position, i) => lidar.calculatePoint(position[0], position[1], position[2], 0, 0, 0, 200, scanAngles[i], true) );
+
+const difference = math.subtract(coordinatesWithErrors, coordinates);
+
+fs.writeFile('./output/coordinates.xyz', coordinatesToCsv(coordinates), (err, result) => !!err ? console.log(result) : console.log(err));
+
+fs.writeFile('./output/coordinatesWithErrors.xyz', coordinatesToCsv(coordinatesWithErrors), (err, result) => !!err ? console.log(result) : console.log(err));
+
+fs.writeFile('./output/difference1.csv', coordinatesToCsv(difference), (err, result) => !!err ? console.log(result) : console.log(err));
